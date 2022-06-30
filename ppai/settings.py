d16b0c3a9c53a6a353315676c4edf70ef247cddc
Leo Cappiello <54512529+Leocappiello@users.noreply.github.com>
2022-06-30 15:03:16 -0300

    Update settings.py

--- ppai/settings.py
+++ ppai/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rjm#t8ap2s5n=bzc+!&!93)$9jqlz8m7+c_@p$h@j8^)u5^33('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
